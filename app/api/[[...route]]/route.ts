709c73c40d5850f57c2bd65405f3229527d2b08d
Gerson Junior <gersoncjunior@gmail.com>
2025-12-25 20:19:19 -0500

    fix: more code

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,13 +2,20 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 const app = new Hono().basePath("/api");
 
 app
-  .get("/hello", (c) => {
+  .get("/hello", clerkMiddleware(), (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({ message: "Unauthorized" }, 401);
+    }
     return c.json({
       message: "Hello Next.js!",
+      userId: auth.userId,
     });
   })
   .get(
